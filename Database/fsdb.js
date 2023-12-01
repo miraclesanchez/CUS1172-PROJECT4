7dabd5ab029e736b07a37ba99b60010d93d16f03
Miracle Sanchez <miracle.sanchez19@stjohns.edu>
2023-11-30 21:51:03 -0500

    added database connection definition module using file system library

---
+++ Database/fsdb.js
@@ -0,0 +1,27 @@
+//allows us to use the file system library
+const fs = require ('fs');
+
+//loads the databse from the filesystem. if it is already there then it loads it, if not it creates one THEN loads it
+var loadDatabase = (db_connection, schema = {}) =>{
+    if(!fs.existsSync(db_connection)){
+        fs.writeFileSync(db_connection,JSON.stringify(schema));
+    }
+
+    let model = require (db_connection);
+
+    //creates the db object
+    var db = {
+        model: model,
+        filename: db_connection,
+        update: () =>{
+            fs.writeFileSync(db_connection,JSON.stringify(model));
+        },
+        addCollection : (collection) => {
+            model['collection'] = [];
+        }
+    }
+    return db;
+}
+
+//allows the method to be used in other modules
+module.exports = loadDatabase;
